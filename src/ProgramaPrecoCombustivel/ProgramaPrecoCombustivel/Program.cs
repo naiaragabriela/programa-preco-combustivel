62d97a9a86a689c8bf1f3b3ad669e055253181c6
naiaragabriela <naigabi31@gmail.com>
2023-03-05 20:11:09 -0300

    calculo do preço de combustivel utilizando funções

---
+++ src/ProgramaPrecoCombustivel/ProgramaPrecoCombustivel/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+float PrecoAlcool()
+{
+    Console.Write("Digite o preço do álcool na sua cidade: ");
+    return float.Parse(Console.ReadLine());
+}
+
+float PrecoGasolina()
+
+{
+    Console.Write("Digite o preço da gasolina na sua cidade: ");
+    return float.Parse(Console.ReadLine());
+}
+
+void Preco()
+{
+    if(PrecoAlcool() >= 0.72 * PrecoGasolina())
+    {
+        Console.WriteLine("Pelos preços dos combustíveis, é aconselhável abastecer com GASOLINA");
+    }
+    else
+    {
+        Console.WriteLine("Pelos preços dos combustíveis, é aconselhável abastecer com Álcool ");
+    }
+}
+
+Preco();
